8ea40edbeb9c55884155811a86789aa79a8874fb
Juyeon Lee <johndoe@example.com>
2022-05-24 12:25:21 +0900

    [Feat] 날짜 및 레벨 별 정렬

---
+++ routes/project_sort.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+const ejs = require('ejs');
+const fs = require('fs');
+const view = fs.readFileSync('./views/project_sort.ejs','utf8');
+
+
+router.get("date", function(req, res) {
+    db.query("SELECT * FROM project ORDER BY proj_date desc", function(err, result, fields){
+        if(err) throw err;
+        else{
+            var page =  ejs.render(view, {
+                title:"Projects",
+                data: result,
+            });
+            res.send(page);
+        }
+    })
+});
+
+router.get("level_1", function(req, res) {
+    db.query("SELECT * FROM project where proj_level=1 ORDER BY proj_date desc", function(err, result, fields){
+        if(err) throw err;
+        else{
+            var page =  ejs.render(view, {
+                title:"Projects",
+                data: result,
+            });
+            res.send(page);
+        }
+    })
+});
+
+router.get("level_2", function (req, res) {
+    db.query("SELECT * FROM project where proj_level=2 ORDER BY proj_date desc", function(err, result, fields){
+        if(err) throw err;
+        else{
+            var page =  ejs.render(view, {
+                title:"Projects",
+                data: result,
+            });
+            res.send(page);
+        }
+    })
+});
+
+router.get("level_3", function (req, res) {
+    db.query("SELECT * FROM project where proj_level=3 ORDER BY proj_date desc", function(err, result, fields){
+        if(err) throw err;
+        else{
+            var page =  ejs.render(view, {
+                title:"Projects",
+                data: result,
+            });
+            res.send(page);
+        }
+    })
+});
+
+/*router.get("skillstacks", function (req, res) {
+    db.query("SELECT * FROM project where proj_level=3 ORDER BY proj_date desc", function(err, result, fields){
+        if(err) throw err;
+        else{
+            var page =  ejs.render(view, {
+                title:"Projects",
+                data: result,
+            });
+            res.send(page);
+        }
+    })
+});*/
+
+module.exports = router;
