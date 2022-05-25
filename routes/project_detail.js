6418cbd1c8c7345d4fb6cd7ab531121d0965d876
ash <sehyeon014@gmail.com>
2022-05-25 14:38:20 +0900

    [Feat] 팀원 등록(participate 테이블 등록)

--- routes/project_detail.js
+++ routes/project_detail.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
+let proj_id = {
+	id: 0
+};
 router.get('/*', function(req,res) {
 	const sql = `SELECT * FROM project WHERE proj_id=?`;
+	proj_id.id = req.params['0'];
 	db.query(sql, [req.params['0']], function (err, results) {
 		if (err) console.log(err);
 			console.log(results[0]);
@@ -22,6 +26,32 @@ router.get('/*', function(req,res) {
 		}
 
 		})
+});
+
+router.post('/*',function(req,res) {
+	const proj_leader=req.session.user['userid'];
+	const proj = proj_id.id;
+	const member=req.body.member;
+	const sql="INSERT INTO participate (part_user, part_project) VALUES (?, ?)";
+	const params=[member,proj];
+	db.query("SELECT * FROM user WHERE user_id = ?", [member], function(err,rows) {    //받는사람이 user테이블에 있는지 확인해야한다.
+		if (rows.length>0) {   //receiver 존재! -> db에 저장
+			 db.query(sql,params,function(err) { 
+					 if (err) console.error("err : " + err);
+					 else{
+							 console.log(params);
+							 res.write(`<script type="text/javascript">alert('member add successfully!')</script>`);
+							 res.write('<script>window.location="/project_sort"</script>');
+					 }
+			 })
+		 }
+	 else{ //receiver존재X
+			 console.log(err);
+			 console.log("member 존재하지 않음");
+			 res.write(`<script type="text/javascript">alert('Member ID does not exit!')</script>`);
+			 res.write('<script>window.location="/project_sort"</script>'); 
+	 }
+}) 
 });
 
 module.exports=router;
