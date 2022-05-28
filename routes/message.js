e07ba5acd56c310a41470fb5f11b2df607b17c6c
leejw-lu <jiwoojenny@naver.com>
2022-05-28 22:33:47 +0900

    [Feat] 송신자,수신자 모두 delete=1이면  db에서도 삭제

--- routes/message.js
+++ routes/message.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
+//쪽지 송신자,수신자 모두 삭제했으면 db에도 삭제하기
+//언제삭제? -> 바로 삭제는 아니고 node app.js 다시하면 삭제된다..
+const sql= "delete FROM message WHERE delete_sender = 1 and delete_receiver= 1 ";
+    db.query(sql, function(err, rows) {
+        if(err) console.error("err: ", err);
+        console.log("deleted from message db");
+    });
+
 router.get('/', function(req,res) {
 
     if(req.session.user){  //로그인 후 쪽지 사용가능
@@ -13,11 +21,14 @@ router.get('/', function(req,res) {
                 user_id: req.session.user['userid'], 
                 rows: rows});
         });
-
+        
     } else{
         res.write(`<script type="text/javascript">alert('Available after login!')</script>`);
         res.write('<script>window.location="/"</script>');
     }
 })
+
+
+
 
 module.exports=router;
