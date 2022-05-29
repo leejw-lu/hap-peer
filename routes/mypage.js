e0d8972789cebd3d45ad7b284de2e3221e1e9cce
Juyeon Lee <johndoe@example.com>
2022-05-30 00:15:43 +0900

    [Fix] 스크랩 없을시 마이페이지 조회 안되는 버그 수정 및 버튼 스타일 수정

--- routes/mypage.js
+++ routes/mypage.js
@@ -8,7 +8,7 @@ const path = require("path");
 router.get('/', function(req,res) {
     //req.session.user는 객체
     if(req.session.user){  //로그인 되어있는가?
-        const sql = "SELECT u.user_info, u.user_stack, u.user_image, p.proj_title, p.proj_id FROM user AS u JOIN scrap AS s ON u.user_id = s.sc_user JOIN project AS p ON s.sc_project = p.proj_id WHERE u.user_id = ?"
+        const sql = "SELECT u.user_info, u.user_stack, u.user_image, p.proj_title, p.proj_id FROM user AS u LEFT OUTER JOIN scrap AS s ON u.user_id = s.sc_user LEFT OUTER JOIN project AS p ON s.sc_project = p.proj_id WHERE u.user_id = ?"
         //user테이블의 정보와 스크랩된 프로젝트 불러오기
         db.query(sql, [req.session.user['userid']], function(err, rows){
             if (err) console.error("err: ", err);
@@ -16,6 +16,7 @@ router.get('/', function(req,res) {
                 user_id: req.session.user['userid'],
                 user_nickname: req.session.user['nickname'], rows: rows});
                 
+               console.log(rows);
             })
         
     } else{  //로그인하지 않고 접근한 경우
