0fe196c0d91e3ebd47c1373d463b5efbfefa340b
ash <sehyeon014@gmail.com>
2022-06-02 12:24:38 +0900

    [Feat] 프로젝트 페이지에서 팀원 정보 확인, 프로젝트 편집 후 해당 프로젝트 페이지로 이동

--- routes/project_detail.js
+++ routes/project_detail.js
@@ -7,23 +7,39 @@ let proj_id = {
 };
 router.get('/*', function(req,res) {
 	const sql = 'SELECT * FROM project WHERE proj_id=?';
+	const sql_parti = 'SELECT * FROM participate WHERE part_project=?';
 	const sql2 = 'SELECT * FROM project LEFT OUTER JOIN scrap ON (proj_id = sc_project and sc_user = ?) where proj_id = ?';
 	proj_id.id = req.params['0'];
-	if (req.session.user){
-		db.query(sql2, [req.session.user['userid'],proj_id.id], function(err, results){
-		return res.render("project_detail", {
-			user_id: req.session.user['userid'],
-			project_detail: results[0]
+	
+	// const sql_insert = 'INSERT INTO evaluation (ev_project, ev) VALUES (?, ?)';
+	// const params_insert = [];
+	// db.query(sql_insert, params_insert, function(err) {
+	// 	if (err) console.log(err);
+	// 	else console.log("eval input success");
+	// })
+	
+	db.query(sql_parti, req.params['0'], function(error, part_res) {
+		if(error) console.log(error);
+		if (req.session.user){
+			db.query(sql2, [req.session.user['userid'],proj_id.id], function(err, results){
+				console.log(part_res);
+				return res.render("project_detail", {
+					user_id: req.session.user['userid'],
+					project_detail: results[0],
+					part_proj: part_res
+				});
+			})}
+			else{
+				db.query(sql, [proj_id.id], function(err, results){
+					return res.render("project_detail", {
+						user_id: "비회원",
+						project_detail: results[0],
+						part_proj: part_res
+					});
+				})
+			}
 		});
-	})}
-	else{
-		db.query(sql, [proj_id.id], function(err, results){
-		return res.render("project_detail", {
-			user_id: "비회원",
-			project_detail: results[0]
-		});
-	})
-}});
+});
 
 router.post('/*',function(req,res) {
 	const proj = proj_id.id;
@@ -37,15 +53,15 @@ router.post('/*',function(req,res) {
 				if (row.length > 0) {
 					console.log("이미 존재하는 멤버");
 					res.write(`<script type="text/javascript">alert('Member already exit!')</script>`);
-					res.write('<script>window.location="/project_sort"</script>'); 
+					res.write(`<script>window.location="/project_detail/${proj_id.id}"</script>`);
 				}
 				else {
 					db.query(sql,params,function(err) { 
 							if (err) console.error("err : " + err);
 							else{
 									console.log(params);
-									res.write(`<script type="text/javascript">alert('member add successfully!')</script>`);
-									res.write('<script>window.location="/project_sort"</script>');
+									res.write(`<script type="text/javascript">alert('Success!')</script>`);
+									res.write(`<script>window.location="/project_detail/${proj_id.id}"</script>`);
 							}
 					})
 				}
@@ -55,7 +71,7 @@ router.post('/*',function(req,res) {
 			 console.log(err);
 			 console.log("member 존재하지 않음");
 			 res.write(`<script type="text/javascript">alert('Member ID does not exit!')</script>`);
-			 res.write('<script>window.location="/project_sort"</script>'); 
+			 res.write(`<script>window.location="/project_detail/${proj_id.id}"</script>`); 
 	 }
 }) 
 });
