5cd451a9d6fb00bec20955f184a4860de6bc3f41
Juyeon Lee <johndoe@example.com>
2022-06-04 04:45:13 +0900

    [Feat] 프로젝트 세부검색 기능 구현

--- routes/project_sort.js
+++ routes/project_sort.js
@@ -24,102 +24,33 @@ router.get("/", function(req, res) {
     })
 });
 
-router.get("/date", function(req, res) {
-    db.query("SELECT * FROM project ORDER BY proj_date desc", function(err, result, fields){
-        if(err) console.log(err);
-
-        if (req.session.user){
-            return res.render("project_sort", {
-				user_id: req.session.user['userid'],
-				title:"Projects",
-                data: result,
-			});
-		}
-		else{
-            return res.render("project_sort", {
-				user_id: "비회원",
-				title:"Projects",
-                data: result,
-			});
-		}
-    })
-});
-
-router.get("/level_1", function(req, res) {
-    db.query("SELECT * FROM project where proj_level=1 ORDER BY proj_date desc", function(err, result, fields){
-        if(err) console.log(err);
-
-        if (req.session.user){
-            return res.render("project_sort", {
-				user_id: req.session.user['userid'],
-				title:"Projects",
-                data: result,
-			});
-		}
-		else{
-            return res.render("project_sort", {
-				user_id: "비회원",
-				title:"Projects",
-                data: result,
-			});
-		}
-    })
-});
-
-router.get("/level_2", function (req, res) {
-    db.query("SELECT * FROM project where proj_level=2 ORDER BY proj_date desc", function(err, result, fields){
-        if(err) console.log(err);
-
-        if (req.session.user){
-            return res.render("project_sort", {
-				user_id: req.session.user['userid'],
-				title:"Projects",
-                data: result,
-			});
-		}
-		else{
-            return res.render("project_sort", {
-				user_id: "비회원",
-				title:"Projects",
-                data: result,
-			});
-		}
-    })
-});
-
-router.get("/level_3", function (req, res) {
-    db.query("SELECT * FROM project where proj_level=3 ORDER BY proj_date desc", function(err, result, fields){
-        if(err) console.log(err);
-
-        if (req.session.user){
-            return res.render("project_sort", {
-				user_id: req.session.user['userid'],
-				title:"Projects",
-                data: result,
-			});
-		}
-		else{
-            return res.render("project_sort", {
-				user_id: "비회원",
-				title:"Projects",
-                data: result,
-			});
-		}
-    })
-});
-
 router.post("/", function (req, res) {
-    const sql = "SELECT * FROM project where proj_stack LIKE ?";
-    const skillstack=req.body.skillstack;
-    db.query(sql, skillstack, function(err, result, fields){
+    const sql = "SELECT * FROM project where (proj_title LIKE ?) and (proj_leader LIKE ?) and (proj_level LIKE ?) AND (proj_stack LIKE ?)";
+    let title = '%'+req.body.title+'%';
+    let teamleader = '%'+req.body.teamleader+'%';
+    let level = req.body.level;
+    let skillstack='%'+req.body.skillstack+'%';
+    if (title=='NULL') title = '';
+    if (teamleader=='NULL') teamleader = '';
+    if (skillstack=='NULL') skillstack = '';
+    const params = [title,teamleader,level,skillstack];
+    db.query(sql, params, function(err, result, fields){
         if(err) throw err;
         else{
-            var page =  ejs.render(view, {
-                user_id: req.session.user['userid'],
-                title:"Projects",
-                data: result,
-            });
-            res.send(page);
+            if (req.session.user){
+                return res.render("project_sort", {
+                    user_id: req.session.user['userid'],
+                    title:"Projects",
+                    data: result,
+                });
+            }
+            else{
+                return res.render("project_sort", {
+                    user_id: "비회원",
+                    title:"Projects",
+                    data: result,
+                });
+            }
         }
     })
 });
