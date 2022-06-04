1c674809ebbeb08afb6600a7ac3c3c922c85b27c
leejw-lu <jiwoojenny@naver.com>
2022-06-05 02:21:07 +0900

    [Feat] 마이페이지에서 사용자 평점Graph 보이기

--- routes/mypage.js
+++ routes/mypage.js
@@ -11,27 +11,44 @@ router.get('/', function(req,res) {
 
     if(req.session.user){  //로그인 되어있는가?
 
-        const sql2 = "SELECT p.proj_title, p.proj_id FROM user AS u LEFT OUTER JOIN scrap AS s ON u.user_id = s.sc_user LEFT OUTER JOIN project AS p ON s.sc_project = p.proj_id WHERE u.user_id = ?"
+        const sql_scrap = "SELECT p.proj_title, p.proj_id FROM user AS u LEFT OUTER JOIN scrap AS s ON u.user_id = s.sc_user LEFT OUTER JOIN project AS p ON s.sc_project = p.proj_id WHERE u.user_id = ?"
         //스크랩된 프로젝트 정보 담긴 rows를 scrap에 대입.
-        db.query(sql2, [req.session.user['userid']],  function(err, rows){
+        db.query(sql_scrap, [req.session.user['userid']],  function(err, rows){
             if (err) console.error("err: ", err);
             scrap= rows;
             console.log(scrap);
         })
 
         const sql = "SELECT u.user_info, u.user_stack, u.user_image, p.proj_title, p.proj_id, p.recruit_status, p.develop_status FROM user AS u LEFT OUTER JOIN participate AS pa ON u.user_id = pa.part_user LEFT OUTER JOIN project AS p ON pa.part_project = p.proj_id WHERE u.user_id = ?";
-        //유저프로필 정보, 참여 정보 받아오기
+        const sql_ev="select avg(ev_value1) as v1, avg(ev_value2) as v2, avg(ev_value3) as v3, avg(ev_value4) as v4, avg(ev_value5) as v5, count(*)as num from evaluation where ev_rated= ? and ev_evaluated=1";
+        // (유저프로필 정보, 참여) + 평점 정보 받아오기
+
         db.query(sql, [req.session.user['userid']], function(err, rows) {
             if(err) console.error("err: ", err);
             console.log(rows);
-
-            //객체key값 userid, nickname views로 넘겨서 회원정보 마이페이지에 보이게하기
-            res.render("mypage",{
-                user_id: req.session.user['userid'],
-                user_nickname: req.session.user['nickname'], 
-                rows: rows,     //유저프로필+ 참여정보 
-                rows2: scrap   //스크랩정보
-            });
+            
+            db.query(sql_ev,[req.session.user['userid']],function(err,result){		//평가table에서 유저정보 확인
+                if (err) console.log(err);
+                if(result.length > 0 ){
+                    res.render("mypage",{
+                        user_id: req.session.user['userid'],
+                        user_nickname: req.session.user['nickname'], 
+                        rows: rows,     //유저프로필+ 참여정보 
+                        rows2: scrap,   //스크랩정보
+                        ev_value: result[0]
+                    });
+                }
+                else{                   //평가table에 피평가id없음 + 평가table에 있지만 아직 평가 전 (이것땜에 join할수없다.)
+                    res.render("mypage",{
+                        user_id: req.session.user['userid'],
+                        user_nickname: req.session.user['nickname'], 
+                        rows: rows,     //유저프로필+ 참여정보 
+                        rows2: scrap,   //스크랩정보
+                        ev_value: "정보없음"
+                    });     
+                }
+    
+            })
         })
 
     } else{     //로그인하지 않고 접근한 경우
