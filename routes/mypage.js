5515c85a21d6442b5d0a7f2eb3272b941c03f7e4
Juyeon Lee <johndoe@example.com>
2022-06-15 01:48:02 +0900

    [Feat] 마이페이지 기술스택 수정완료

--- routes/mypage.js
+++ routes/mypage.js
@@ -20,7 +20,7 @@ router.get('/', function (req, res) {
       console.log(scrap);
     })
 
-    const sql = "SELECT u.user_info, u.user_stack, u.user_image, p.proj_title, p.proj_id, p.recruit_status, p.develop_status FROM user AS u LEFT OUTER JOIN participate AS pa ON u.user_id = pa.part_user LEFT OUTER JOIN project AS p ON pa.part_project = p.proj_id WHERE u.user_id = ?";
+    const sql = "SELECT u.user_info, u.user_stack, u.user_stacketc, u.user_image, p.proj_title, p.proj_id, p.recruit_status, p.develop_status FROM user AS u LEFT OUTER JOIN participate AS pa ON u.user_id = pa.part_user LEFT OUTER JOIN project AS p ON pa.part_project = p.proj_id WHERE u.user_id = ?";
     const sql_evF = "select count(*) as num from evaluation where ev_rater=? and ev_evaluated= 1 and ev_project= ? "
     const sql_part = "select count(*) as num from participate where part_project= ? "
     const sql_ev = "select avg(ev_value1) as v1, avg(ev_value2) as v2, avg(ev_value3) as v3, avg(ev_value4) as v4, avg(ev_value5) as v5, count(*)as num from evaluation where ev_rated= ? and ev_evaluated=1";
