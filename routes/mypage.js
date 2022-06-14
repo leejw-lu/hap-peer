3ba3b1f5043eff0079099cd6f563cda379771c93
leejw-lu <jiwoojenny@naver.com>
2022-06-14 22:28:39 +0900

    [Mod] 평가하기+평가완료 상태 sql문 변경

--- routes/mypage.js
+++ routes/mypage.js
@@ -21,23 +21,23 @@ router.get('/', function (req, res) {
     })
 
     const sql = "SELECT u.user_info, u.user_stack, u.user_image, p.proj_title, p.proj_id, p.recruit_status, p.develop_status FROM user AS u LEFT OUTER JOIN participate AS pa ON u.user_id = pa.part_user LEFT OUTER JOIN project AS p ON pa.part_project = p.proj_id WHERE u.user_id = ?";
-    const sql_evF = "select count(*) as num from evaluation where ev_rater=? and ev_evaluated= 1 and ev_project= ? "
-    const sql_part = "select count(*) as num from participate where part_project= ? "
+    const sql_evcount = "select count(*) as num from evaluation where ev_rater=? and ev_evaluated= 1 and ev_project= ? "     //평가한 팀원명수
+    const sql_toev = "select count(*) as num from evaluation where ev_rater= ? and ev_project=? "     //평가해야할 팀원명수
     const sql_ev = "select avg(ev_value1) as v1, avg(ev_value2) as v2, avg(ev_value3) as v3, avg(ev_value4) as v4, avg(ev_value5) as v5, count(*)as num from evaluation where ev_rated= ? and ev_evaluated=1";
 
     db.query(sql, [req.session.user['userid']], function (err, rows) {
       if (err) console.error(err);
       for (let i = 0; i < rows.length; i++) {
         partPjID[i] = rows[i].proj_id;
-        db.query(sql_evF, [req.session.user['userid'], partPjID[i]], function (err, Evaluated) {
+        db.query(sql_evcount, [req.session.user['userid'], partPjID[i]], function (err, Evaluated) {
           if (err) console.error(err);
           numEvaluated = Evaluated[0].num;
           console.log("numEvaluated:" + numEvaluated);
         })
-        db.query(sql_part, [partPjID[i]], function (err, PartPeer) {
+        db.query(sql_toev, [req.session.user['userid'], partPjID[i]], function (err, PartPeer) {
           if (err) console.error(err);
           numPartPeer = PartPeer[0].num;
-          if (numEvaluated == (numPartPeer - 1)) {
+          if (numEvaluated == numPartPeer) {  //평가테이블에서 해당프로젝트에 참여한 사용자가 평가한 팀원수== 해당프로젝트에 참여한 팀원수(자신제외)
             statusEvaluated[i] = "평가완료"
           }
           else {
